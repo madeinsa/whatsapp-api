3abb448b9c688aa9a5204ac10adbfbead50cfc45
CyberPanel <support@cyberpanel.net>
2025-03-26 09:20:37 +0000

    Fix The Property new OnWhatsAppDto(result.jid, result.exists as boolean)

--- src/whatsapp/services/whatsapp.service.ts
+++ src/whatsapp/services/whatsapp.service.ts
@@ -1926,7 +1926,7 @@ export class WAStartupService {
       } else {
         try {
           const result = (await this.client.onWhatsApp(jid))[0];
-          onWhatsapp.push(new OnWhatsAppDto(result.jid, !!result.exists));
+          onWhatsapp.push(new OnWhatsAppDto(result.jid, !!result.exists as boolean));
         } catch (error) {
           onWhatsapp.push(new OnWhatsAppDto(number, false));
         }
